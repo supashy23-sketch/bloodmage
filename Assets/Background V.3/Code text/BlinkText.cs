70721e04cd38ba407feb96f1fe8d9ccec283ad20
beem420012 <kittipop.thon@bumail.net>
2025-10-19 03:00:14 +0700

    ep3
    UI menu

---
+++ Assets/Background V.3/Code text/BlinkText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour
+{
+    public Text targetText; // ลาก Text UI มาวางใน Inspector
+    public float blinkSpeed = 1f; // ความเร็วในการกระพริบ (1 = ช้า, 5 = เร็ว)
+
+    void Update()
+    {
+        if (targetText != null)
+        {
+            // เปลี่ยนความโปร่งใสตามเวลา (sin wave)
+            Color c = targetText.color;
+            c.a = Mathf.Abs(Mathf.Sin(Time.time * blinkSpeed));
+            targetText.color = c;
+        }
+    }
+}
